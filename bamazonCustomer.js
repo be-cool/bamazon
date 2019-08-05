42edc7743998ff24e0e92445349f38686612b1d7
Ben Cooley <bcooley58@gmail.com>
2019-08-05 17:21:45 -0400

    updated with secondary prompt for how many items they would like to purchase.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,38 +3,46 @@ var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "740Washington",
-  database: "bamazon"
+    // Your password
+    password: "740Washington",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     start();
 })
 
-function start () {
+function start() {
     // pull in the items to be able to push them into a list for the customer to see in the CLI
-    connection.query("SELECT * FROM products", function(err, results) {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-    inquirer.prompt({
-        name: "itemSelect",
-        type: "list",
-        message: "What item would you like to purchase today?",
-        choices: function() {
-            const itemArray = [];
-            for (var i = 0; i<results.length; i++) {
-                itemArray.push(results[i].product_name);
+        inquirer.prompt([
+            {
+                name: "itemSelect",
+                type: "list",
+                message: "What item would you like to purchase today?",
+                choices: function () {
+                    const itemArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        itemArray.push(results[i].product_name);
+                    }
+                    return itemArray;
+                }
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to purchase?"
             }
-            return itemArray;
-        }
+        ])
     })
-    })};
+};
